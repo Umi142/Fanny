ca95b970a30be37c8a85e66ad10757b0adff53ca
Yumilka Gutierrez <ypgutierrez@student.apc.edu.ph>
2026-02-17 13:47:34 +0000

    Test Frontend 2

--- pinoy-joke-wall/frontend/vite.config.ts
+++ pinoy-joke-wall/frontend/vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,4 +8,4 @@ export default defineConfig({
       '/api': 'https://cuddly-goldfish-q7x7pw7j5xw6247j-3000.app.github.dev'
     }
   }
-})
+});
